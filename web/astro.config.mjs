7847d1d67b5739a91d022ad5e3a2ff9ddfae9b9d
Alicia Sykes <alicia@omg.lol>
2024-01-28 22:04:40 +0000

    Set Astro SITE and BASE_URL, don't add slash

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,5 +5,6 @@ import lit from "@astrojs/lit";
 // https://astro.build/config
 export default defineConfig({
   integrations: [lit()],
-  base: process.env.BASE_URL || '/',
+  site: process.env.SITE,
+  base: process.env.BASE_URL,
 });
