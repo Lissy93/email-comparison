4a96838d7d7d60cf5219e4856f14ed89a907085c
Alicia Sykes <alicia@omg.lol>
2024-01-28 20:53:23 +0000

    Update setting of Base URL

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,4 +6,9 @@ import lit from "@astrojs/lit";
 export default defineConfig({
   integrations: [lit()],
   base: process.env.BASE_URL || '/',
+  vite: {
+    define: {
+      'import.meta.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+    },
+  },
 });
