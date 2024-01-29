f934562cf17cb1b12ad18eb685f69116bc4b8c1d
Alicia Sykes <alicia@omg.lol>
2024-01-29 12:55:08 +0000

    Adds sitemap, updates banner, sets SITE in GH workflow

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-import lit from "@astrojs/lit";
+import lit from '@astrojs/lit';
+
+const siteMapFilter = (page) => !page.includes('/summary');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit()],
+  integrations: [lit(), sitemap({ filter: siteMapFilter })],
   site: process.env.SITE,
   base: process.env.BASE_URL,
 });
